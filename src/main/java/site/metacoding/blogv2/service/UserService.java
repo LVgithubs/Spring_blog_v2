349faf1e7c1eec92d5c8debdc445126d2fd3631f
LV <whgodbs32191@gmail.com>
2022-03-22 16:58:36 +0900

    6.로그인 완료

--- src/main/java/site/metacoding/blogv2/service/UserService.java
+++ src/main/java/site/metacoding/blogv2/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.domain.user.UserRepository;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 import site.metacoding.blogv2.web.api.dto.user.LoginDto;
@@ -21,9 +22,11 @@ public class UserService {
     }
 
     @Transactional
-    public void 로그인(LoginDto loginDto) {
+    public User 로그인(LoginDto loginDto) {
         // save하면 db에 insert하고 insert된 결과를 다시 return 해준다.
-        userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
+        User userEntity = userRepository.mLogin(loginDto.getUsername(), loginDto.getPassword());
+        return userEntity;
+
     }
 
 }
--- src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
@@ -1,10 +1,13 @@
 package site.metacoding.blogv2.web.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
@@ -14,6 +17,7 @@ import site.metacoding.blogv2.web.api.dto.user.LoginDto;
 @RestController
 public class UserApiController {
     private final UserService userService;
+    private final HttpSession session;
 
     // spring 기본 전략이 x-www-f 이기 때문에,
     // json 이 아니기 때문에 join(JoinDto joinDto) 로는 데이터를 받을 수 없다.
@@ -29,9 +33,12 @@ public class UserApiController {
     @PostMapping("/api/login")
     public ResponseDto<String> login(@RequestBody LoginDto loginDto) {
 
-        userService.로그인(loginDto);
-
-        return new ResponseDto<String>(1, "로그인 성공", null);
+        User userEntity = userService.로그인(loginDto);
+        if (userEntity == null) {
+            return new ResponseDto<String>(-1, "로그인실패", null);
+        }
+        session.setAttribute("principal", userEntity);
+        return new ResponseDto<String>(1, "로그인성공", null);
     }
 
 }
